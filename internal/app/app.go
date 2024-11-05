0fe8de13a9bcef0fca2da0099f5af2f4591cc4ea
merki86 <nassen.d230@gmail.com>
2024-11-05 20:36:24 +0500

    fix: no need in serving static files

--- internal/app/app.go
+++ internal/app/app.go
@@ -8,8 +8,6 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"strings"
 	"syscall"
 	"time"
 
@@ -85,11 +83,6 @@ func Run() {
 
 	routes.RegisterRoutes(r, stg, log)
 
-	// Load static files
-	workDir, _ := os.Getwd()
-	filesDir := http.Dir(filepath.Join(workDir, "./web/static"))
-	FileServer(r, "/web/static", filesDir)
-
 	srv := server.New(cfg, r)
 	go func() {
 		if err := srv.Run(); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -126,27 +119,6 @@ func Run() {
 	}
 
 	log.Info("Server has been shut down")
-}
-
-// FileServer conveniently sets up a http.FileServer handler to serve
-// static files from a http.FileSystem.
-func FileServer(r chi.Router, path string, root http.FileSystem) {
-	if strings.ContainsAny(path, "{}*") {
-		panic("FileServer does not permit any URL parameters.")
-	}
-
-	if path != "/" && path[len(path)-1] != '/' {
-		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
-		path += "/"
-	}
-	path += "*"
-
-	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
-		rctx := chi.RouteContext(r.Context())
-		pathPrefix := strings.TrimSuffix(rctx.RoutePattern(), "/*")
-		fs := http.StripPrefix(pathPrefix, http.FileServer(root))
-		fs.ServeHTTP(w, r)
-	})
 }
 
 func setupLogger(env string) *slog.Logger {
